5b6f83e036caef620cf7f139110a6d2353dbdf84
Nariman Avatov <110205226+nAvatov@users.noreply.github.com>
2023-09-04 18:56:24 +0300

    Resolved import dependencies path's

--- StartUp.go
+++ StartUp.go
@@ -4,6 +4,8 @@ import (
 	onGoingKatas "codewars/src/in_process"
 )
 
+//onGoingKatas "src/in_process"
+
 func main() {
 	onGoingKatas.TestBuddyPairs()
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module codewars
-
-go 1.21.0
